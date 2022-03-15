0e9247c9407d2491cd7a8bbb1cfe405f2af392a6
Ethan-Zhang <ethanzhang55@outlook.com>
2022-03-15 15:41:01 +0800

    add gunicorn deployment & merge permission_utils
    use geventwebsocket WSGI server & add handling exception for permisson_utils
    
    repo monitor debug
    
    deployment update & modify worker requirement

--- radiaTest-server/server/utils/permission_utils.py
+++ radiaTest-server/server/utils/permission_utils.py
@@ -1,9 +1,68 @@
 import json
 import requests
+from typing import List
 
-from flask import current_app
+from flask import current_app, jsonify
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
+from server import db
 from server.utils.response_util import RET
+from server.model.permission import ReScopeRole, Role, Scope
+from server.utils.response_util import RET
+from server.utils.db import Insert, Precise
+
+
+class PermissionManager:
+    def insert_scope(self, scope_datas):
+        scope_ids = []
+        for sdata in scope_datas:
+            try:
+                scope_id = Insert(Scope, sdata).insert_id(Scope, '/scope')
+                scope_ids.append(scope_id)
+            except (IntegrityError, SQLAlchemyError) as e:
+                current_app.logger.error(str(e))
+                continue
+        return scope_ids
+
+    def generate(self, scope_datas_allow, scope_datas_deny, permission_type, owner_id = None):
+        role = Precise(
+            Role,
+            {
+                "name": "root" if permission_type == "public" else owner_id,
+                "type": permission_type
+            },
+        ).first()
+
+        if not role:
+            return jsonify(error_code=RET.NO_DATA_ERR, error_msg="Role has not been exist")
+    
+        scope_allow_ids = self.insert_scope(scope_datas_allow)
+        _ = self.insert_scope(scope_datas_deny)
+
+        try:
+            for _id in scope_allow_ids:
+                scope_role_data = {
+                    "scope_id": _id,
+                    "role_id": role.id
+                }
+                Insert(ReScopeRole, scope_role_data).insert_id()
+
+        except (SQLAlchemyError, IntegrityError) as e:
+            raise RuntimeError(str(e)) from e
+    
+    def clean(self, uri_part, item_ids: List[int]):
+        try:
+            for item_id in item_ids:
+                filter_str = uri_part + str(item_id)
+                filter_params = []
+                filter_params.append(Scope.uri.like(f'%{filter_str}%'))
+                scopes = Scope.query.filter(*filter_params).all()
+                for scope in scopes:
+                    db.session.delete(scope)
+                    db.session.commit()
+                    
+        except (SQLAlchemyError, IntegrityError) as e:
+            raise RuntimeError(str(e)) from e
 
 
 class PermissionItemsPool:
