87baa17022232aa7da72f436c4b18404f4c0308d
Ethan-Zhang <ethanzhang55@outlook.com>
2022-04-19 22:30:38 +0800

    debug
    debug
    
    doc add
    
    debug

--- build/scripts/web/install_nodejs.sh
+++ build/scripts/web/install_nodejs.sh
@@ -9,34 +9,55 @@ basearch=$(uname -m)
 nodejs_version=$(node -v 2>/dev/null)
 
 # check nodejs
-if [[ -n "$nodejs_version" && "$nodejs_version" = "$node_req_v" ]];then
-    echo "nodejs $node_req_v has been prepared"
+if [[ -n "${nodejs_version}" && "${nodejs_version}" = "${node_req_v}" ]];then
+    echo "nodejs ${node_req_v} has been prepared"
     exit
-elif [[ -n "$nodejs_version" ]]; then
-    read -p "nodejs $nodejs_version is not appropriate for radiaTest, $node_req_v is going to replace this version, [Y/n]?"
-    if [[ "$REPLY" != [YyNn] ]];then
-        echo "you should enter Y/y or N/n"
-        exit
-    elif [[ "$REPLY" == [Nn] ]];then
-        exit
-    else
-        dnf remove -y nodejs || ( echo "remove nodejs $nodejs_version failed" && exit )
-        echo "nodjs $nodejs_version has been removed"
-    fi
+elif [[ -n "${nodejs_version}" ]]; then
+    n=3
+    while [ $n -ge 0 ];do
+
+        read -p "nodejs ${nodejs_version} is not appropriate for radiaTest, ${node_req_v} is going to replace this version, [Y|N]?"
+        if [[ "${REPLY}" != [YyNn] ]];then
+            echo "you should enter Y/y or N/n"
+        elif [[ "${REPLY}" == [Nn] ]];then
+            exit
+        else
+            dnf remove -y nodejs || ( echo "remove nodejs ${nodejs_version} failed" && exit )
+            echo "nodjs ${nodejs_version} has been removed"
+            break
+        fi
+
+        if [ $n = 0 ];then
+            exit
+        fi
+
+        let n--
+    done
 else
-    read -p "no nodejs installed, nodejs $node_req_v is going to be installed from https://nodejs.org, [Y/n]?"
-    if [[ "$REPLY" != [YyNn] ]];then
-        echo "you should enter Y/y or N/n"
-        exit
-    elif [[ "$REPLY" == [Nn] ]];then
-        exit
-    fi
+    n=3
+    while [ $n -ge 0 ];do
+
+        read -p "no nodejs installed, nodejs ${node_req_v} is going to be installed from https://nodejs.org, [Y|N]?"
+        if [[ "${REPLY}" != [YyNn] ]];then
+            echo "you should enter Y/y or N/n"
+        elif [[ "${REPLY}" == [Nn] ]];then
+            exit
+        else
+            break
+        fi
+        
+        if [ $n = 0 ];then
+            exit
+        fi
+
+        let n--
+    done
 fi
 
-# prepare nodejs v16.14.2
-cd "$workdir"
+# prepare nodejs
+cd "${workdir}"
 
-if [ "$basearch" = "x86_64" ];then
+if [ "{$basearch}" = "x86_64" ];then
     basearch="x64"
 else
     basearch="arm64"
@@ -44,26 +65,26 @@ fi
 
 dnf install -y tar || exit
 
-wget https://npmmirror.com/mirrors/node/$node_req_v/node-$node_req_v-linux-"$basearch".tar.xz || exit \
-    && tar -xJf node-$node_req_v-linux-"$basearch".tar.xz || exit \
-    && rm -rf node-$node_req_v-linux-"$basearch".tar.xz || exit
+wget https://npmmirror.com/mirrors/node/"${node_req_v}"/node-"${node_req_v}"-linux-"${basearch}".tar.xz || exit \
+    && tar -xJf node-"${node_req_v}"-linux-"${basearch}".tar.xz || exit \
+    && rm -rf node-"${node_req_v}"-linux-"${basearch}".tar.xz || exit
 
-ln -s "$workdir"/node-$node_req_v-linux-"$basearch"/bin/node "$workdir"/node
-ln -s "$workdir"/node-$node_req_v-linux-"$basearch"/bin/npm "$workdir"/npm
-ln -s "$workdir"/node-$node_req_v-linux-"$basearch"/bin/npx "$workdir"/npx
+ln -s "${workdir}"/node-"${node_req_v}"-linux-"${basearch}"/bin/node "${workdir}"/node
+ln -s "${workdir}"/node-"${node_req_v}"-linux-"${basearch}"/bin/npm "${workdir}"/npm
+ln -s "${workdir}"/node-"${node_req_v}"-linux-"${basearch}"/bin/npx "${workdir}"/npx
 
 node=$(node -v)
 npm=$(npm -v)
 npx=$(npx -v)
 
 # check version
-if [[ "$node" != "$node_req_v" && "$npm" != "$npm_req_v" && "$npx" != "$npx_req_v" ]]; then
-    echo "Internal Error: Install Nodejs $node_req_v Failed."
+if [[ "${node}" != "${node_req_v}" && "${npm}" != "${npm_req_v}" && "${npx}" != "${npx_req_v}" ]]; then
+    echo "Internal Error: Install Nodejs ${node_req_v} Failed."
     echo "clean environment..."
-    rm -rf "$workdir"/node \
-        && rm -rf "$workdir"/npm \
-        && rm -rf "$workdir"/npx \
-        && rm -rf "$workdir"/node-v16.14.2-linux-"$basearch"
+    rm -rf "${workdir}"/node \
+        && rm -rf "${workdir}"/npm \
+        && rm -rf "${workdir}"/npx \
+        && rm -rf "${workdir}"/node-"${node_req_v}"-linux-"${basearch}"
 else
-    echo "nodejs $node_req_v has been prepared"
+    echo "nodejs ${node_req_v} has been prepared"
 fi
