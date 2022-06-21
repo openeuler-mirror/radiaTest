436834ca118dc9bcb74553ea2d56db3604703f31
MDS_ZHR <w331884949@163.com>
2022-06-21 17:24:19 +0800

    添加修改物理机延期delay接口

--- radiaTest-server/server/apps/pmachine/__init__.py
+++ radiaTest-server/server/apps/pmachine/__init__.py
@@ -17,7 +17,7 @@
 
 from flask_restful import Api
 
-from .routes import PmachineEvent, PmachineItemEvent
+from .routes import PmachineEvent, PmachineItemEvent,PmachineDelayEvent
 from .routes import Power
 from .routes import Install
 from .routes import MachineGroupEvent
@@ -28,8 +28,9 @@ from .routes import MachineGroupHeartbeatEvent
 def init_api(api: Api):
     api.add_resource(PmachineEvent, "/api/v1/pmachine")
     api.add_resource(PmachineItemEvent, "/api/v1/pmachine/<int:pmachine_id>")
+    api.add_resource(PmachineDelayEvent, "/api/v1/pmachine/<int:pmachine_id>/delay")
     api.add_resource(Power, "/api/v1/pmachine/power")
     api.add_resource(Install, "/api/v1/pmachine/install")
-    api.add_resource(MachineGroupEvent, "/api/v1/machine-group")
-    api.add_resource(MachineGroupItemEvent, "/api/v1/machine-group/<int:machine_group_id>")
-    api.add_resource(MachineGroupHeartbeatEvent, "/api/v1/machine-group/heartbeat")
+    api.add_resource(MachineGroupEvent, "/api/v1/machine_group")
+    api.add_resource(MachineGroupItemEvent, "/api/v1/machine_group/<int:machine_group_id>")
+    api.add_resource(MachineGroupHeartbeatEvent, "/api/v1/machine_group/heartbeat")
