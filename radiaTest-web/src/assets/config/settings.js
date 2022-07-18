4e69464b79098277aec72d6d4a473d2bd109cb28
Ethan-Zhang <ethanzhang55@outlook.com>
2022-07-18 11:31:07 +0800

    修复前端代码合入导致的配置文件回退

--- radiaTest-web/src/assets/config/settings.js
+++ radiaTest-web/src/assets/config/settings.js
@@ -2,16 +2,16 @@ const name = 'radiaTest';
 const version = '1.1.0';
 const license = 'Mulan PSL v2';
 
-
 const serverPath = 'radiatest.openeuler.org';
-// const serverPath = '0.0.0.0:8080';
+const websocketProtocol = 'wss';
 
-const newsSocketPath = `wss://${serverPath}/api/v1/msg`;
+const newsSocketPath = `${websocketProtocol}://${serverPath}/message`;
 
 export default {
   name,
   version,
   license,
   serverPath,
+  websocketProtocol,
   newsSocketPath,
 };
