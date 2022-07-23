f272740644208f91d72d23d39341d9444cd48a64
Ethan-Zhang <ethanzhang55@outlook.com>
2022-07-24 04:10:05 +0800

    去除VmachineBriefSchema中的ip字段格式校验

--- radiaTest-server/server/schema/vmachine.py
+++ radiaTest-server/server/schema/vmachine.py
@@ -296,9 +296,3 @@ class VmachineBriefSchema(BaseModel):
     description: str
     milestone: str
     status: str
-
-    @validator("ip")
-    def check_ip_format(cls, v):
-        if not ip_type(v):
-            raise ValueError("this ip of machine group format error")
-        return v
