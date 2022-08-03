3c51023b38385acc513bda7395c75956dcc21e87
xujie <1254023616@qq.com>
2022-08-03 11:04:45 +0800

    测试框架新增筛选；虚拟机筛选BUG修复；选择按钮调整；
    解决代码过长

--- radiaTest-web/src/components/CRUD/SelectionButton.vue
+++ radiaTest-web/src/components/CRUD/SelectionButton.vue
@@ -1,53 +1,23 @@
 <template>
-  <n-button
-    class="selection"
-    text
-    v-show="!showSelection"
-    @click="handleShowSelection"
-  >
-    选择
-  </n-button>
-  <n-button
-    class="selection"
-    text
-    v-show="showSelection"
-    @click="handleOffSelection"
-  >
-    隐藏
-  </n-button>
+  <div class="selectionWrap">
+    <n-icon size="16"><ChecklistFilled /></n-icon>&nbsp;
+    <n-button class="selection" text v-show="!showSelection" @click="handleShowSelection">
+      选择
+    </n-button>
+    <n-button class="selection" text v-show="showSelection" @click="handleOffSelection">
+      隐藏
+    </n-button>
+  </div>
 </template>
 
 <script>
-import { ref, onMounted, defineComponent } from 'vue';
+import { ref, defineComponent } from 'vue';
+import { ChecklistFilled } from '@vicons/material';
 
 export default defineComponent({
-  props: {
-    left: {
-      type: Number,
-      default: 42,
-    },
-    top: {
-      type: Number,
-      default: 145,
-    },
-  },
+  components: { ChecklistFilled },
   setup(props, context) {
     const showSelection = ref(false);
-
-    onMounted(() => {
-      document.getElementsByClassName(
-        'selection'
-      )[0].style.left = `${props.left}px`;
-      document.getElementsByClassName(
-        'selection'
-      )[0].style.top = `${props.top}px`;
-      document.getElementsByClassName(
-        'selection'
-      )[1].style.left = `${props.left}px`;
-      document.getElementsByClassName(
-        'selection'
-      )[1].style.top = `${props.top}px`;
-    });
 
     return {
       showSelection,
@@ -58,15 +28,18 @@ export default defineComponent({
       handleOffSelection() {
         context.emit('off');
         showSelection.value = false;
-      },
+      }
     };
-  },
+  }
 });
 </script>
 
-<style scope>
-.selection {
-  z-index: 1;
-  position: relative;
+<style scope lang="less">
+.selectionWrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  cursor: pointer;
 }
 </style>
--- radiaTest-web/src/views/caseManage/frameWork/frameWork.vue
+++ radiaTest-web/src/views/caseManage/frameWork/frameWork.vue
@@ -1,50 +1,38 @@
 <template>
   <div style="padding:30px">
-    <div style="margin-bottom: 10px">
-      <n-button @click="addFramework" type="primary">
-        添加测试框架
-        <template #icon>
-          <n-icon><Add /></n-icon>
-        </template>
-      </n-button>
-    </div>
+    <n-grid>
+      <n-gi :span="23">
+        <div style="margin-bottom: 10px">
+          <div>
+            <create-button title="添加测试框架" @click="addFramework" />
+          </div>
+        </div>
+      </n-gi>
+      <n-gi :span="1" class="rightGiWrap">
+        <div class="titleBtnWrap">
+          <filterButton :filterRule="filterRule" @filterchange="filterchange"></filterButton>
+        </div>
+      </n-gi>
+    </n-grid>
     <n-data-table
       :row-props="frameRowProps"
       :pagination="frameworkPagination"
       :columns="frameworkColumns"
-      :data="frameworkData"
+      :data="frameworkFilterData"
       :row-key="(row) => row.id"
-      :loading = "frameLoading"
+      :loading="frameLoading"
       remote
     />
-    <n-modal
-      v-model:show="showModal"
-      preset="dialog"
-      :title="isCreate ? '新增测试框架' : '修改测试框架'"
-    >
-      <n-form
-        ref="formRef"
-        label-placement="top"
-        :model="frameworkForm"
-        :rules="frameworkRules"
-      >
+    <n-modal v-model:show="showModal" preset="dialog" :title="isCreate ? '新增测试框架' : '修改测试框架'">
+      <n-form ref="formRef" label-placement="top" :model="frameworkForm" :rules="frameworkRules">
         <n-form-item label="名称" path="name">
-          <n-input
-            v-model:value="frameworkForm.name"
-            placeholder="请输入名称"
-          />
+          <n-input v-model:value="frameworkForm.name" placeholder="请输入名称" />
         </n-form-item>
         <n-form-item label="仓库地址" path="url">
           <n-input v-model:value="frameworkForm.url" placeholder="仓库地址" />
         </n-form-item>
-        <n-form-item
-          label="日志目录相对路径(相对仓库地址根路径)"
-          path="logs_path"
-        >
-          <n-input
-            v-model:value="frameworkForm.logs_path"
-            placeholder="日志目录相对路径"
-          />
+        <n-form-item label="日志目录相对路径(相对仓库地址根路径)" path="logs_path">
+          <n-input v-model:value="frameworkForm.logs_path" placeholder="日志目录相对路径" />
         </n-form-item>
         <n-form-item label="是否已适配" path="adaptive">
           <n-switch v-model:value="frameworkForm.adaptive">
@@ -68,14 +56,29 @@
 </template>
 <script>
 import { modules } from './modules/index';
-import { Add } from '@vicons/ionicons5';
+import Common from '@/components/CRUD';
+import filterButton from '@/components/filter/filterButton.vue';
+
 export default {
-  components: { Add },
+  components: { ...Common, filterButton },
   setup() {
     modules.initData();
+
     return {
       ...modules
     };
   }
 };
 </script>
+
+<style lang="less" scoped>
+.rightGiWrap {
+  display: flex;
+  align-items: center;
+
+  .titleBtnWrap {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
