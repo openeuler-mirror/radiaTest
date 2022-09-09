8cfdd6eec38a9f07a55144a668fe6444656c3a0d
xujie <1254023616@qq.com>
2022-09-09 10:44:27 +0800

    feat:checklist功能

--- radiaTest-web/src/api/put.js
+++ radiaTest-web/src/api/put.js
@@ -1,6 +1,6 @@
 import { modifySuccessMsg, unkonwnErrorMsg } from '@/assets/utils/description';
 import axios from '@/axios';
-function putRequest (url, data) {
+function putRequest(url, data) {
   return new Promise((resolve, reject) => {
     axios
       .put(url, data)
@@ -14,48 +14,52 @@ function putRequest (url, data) {
       });
   });
 }
-export function modifyRepo (id, data) {
+export function modifyRepo(id, data) {
   return putRequest(`/v1/git-repo/${id}`, data);
 }
-export function modifyVmachineIp (id, data) {
+export function modifyVmachineIp(id, data) {
   return putRequest(`/v1/vmachine/${id}/ipaddr`, data);
 }
-export function modifyVmachineSsh (id, data) {
+export function modifyVmachineSsh(id, data) {
   return putRequest(`/v1/vmachine/${id}/ssh`, data);
 }
-export function modifyDelayTime (id, data) {
+export function modifyDelayTime(id, data) {
   return putRequest(`/v1/vmachine/${id}/delay`, data);
 }
-export function modifyPmachineDelayTime (id, data) {
+export function modifyPmachineDelayTime(id, data) {
   return putRequest(`/v1/pmachine/${id}/delay`, data);
 }
-export function modifyPmachineBmc (id, data) {
+export function modifyPmachineBmc(id, data) {
   return putRequest(`/v1/pmachine/${id}/bmc`, data);
 }
-export function modifyPmachineSsh (id, data) {
+export function modifyPmachineSsh(id, data) {
   return putRequest(`/v1/pmachine/${id}/ssh`, data);
 }
-export function modifyCommitStatus (id, data) {
+export function modifyCommitStatus(id, data) {
   return putRequest(`/v1/case/commit/${id}`, data);
 }
-export function modifyCommitStatusBatch (data) {
+export function modifyCommitStatusBatch(data) {
   return putRequest('/v1/case/commit/status', data);
 }
-export function modifyCommitInfo (id,data) {
+export function modifyCommitInfo(id, data) {
   return putRequest(`/v1/case/commit/${id}`, data);
 }
-export function modifyMachineGroup(id,data){
-  return putRequest(`/v1/machine-group/${id}`,data);
+export function modifyMachineGroup(id, data) {
+  return putRequest(`/v1/machine-group/${id}`, data);
 }
-export function modifyGroupUserRole (id, data) {
+export function modifyGroupUserRole(id, data) {
   return putRequest(`/v1/user-role/group/${id}`, data);
 }
-export function organizationInfo (id, data) {
+export function organizationInfo(id, data) {
   return putRequest(`/v1/admin/org/${id}`, data);
 }
-export function milestoneNext (id, data) {
+export function milestoneNext(id, data) {
   return putRequest(`/v1/qualityboard/${id}`, data);
 }
-export function milestoneRollback (id) {
+export function milestoneRollback(id) {
   return putRequest(`/v1/qualityboard/${id}/rollback`);
+}
+
+export function updateCheckListItem(id, data) {
+  return putRequest(`/v1/checklist/${id}`, data);
 }
