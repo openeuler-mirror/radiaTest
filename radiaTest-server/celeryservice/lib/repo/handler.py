829a59ebbb7aae3f675409effd80e0bbd13bbbbe
Ethan-Zhang <ethanzhang55@outlook.com>
2023-04-27 09:56:42 +0800

    to: 去除仓库同步循环gitClone逻辑&&补全日志打印

--- radiaTest-server/celeryservice/lib/repo/handler.py
+++ radiaTest-server/celeryservice/lib/repo/handler.py
@@ -2,7 +2,6 @@ import os
 import shutil
 import shlex
 import subprocess
-from time import sleep
 
 from celeryservice.sub_tasks import update_suite
 from celeryservice.lib import TaskHandlerBase
@@ -10,21 +9,19 @@ from .suite2cases_resolver import Resolver
 
 
 class RepoTaskHandler(TaskHandlerBase):
-    RETRY_LIMIT = 300
-    SLEEP_INTERVAL = 1
-
     def __init__(self, logger, promise):
         self.promise = promise
         super().__init__(logger)
 
     def _git_clone(self, url, branch, oet_path):
-        exitcode, _ = subprocess.getstatusoutput(
+        exitcode, output = subprocess.getstatusoutput(
             "git clone -b {} {}.git {}".format(
                 url,
                 shlex.quote(branch),
                 shlex.quote(oet_path),
             )
         )
+        self.logger.info(output)
         return False if exitcode else True
 
     def main(self, id, name, url, branch, framework_name):
@@ -41,18 +38,7 @@ class RepoTaskHandler(TaskHandlerBase):
         if os.path.isdir(oet_path):
             shutil.rmtree(oet_path)
 
-        count = RepoTaskHandler.RETRY_LIMIT
-        while count > 0:
-            _ = self._git_clone(url, branch, oet_path)
-            if _ is True:
-                break
-            count -= 1
-            sleep(RepoTaskHandler.SLEEP_INTERVAL)
-        
-        if count == 0:
-            raise RuntimeError(
-                f"has tried 30 times to git clone {url}, perhaps influenced by connection error"
-            )
+        self._git_clone(url, branch, oet_path)
 
         self.next_period()
         self.promise.update_state(
