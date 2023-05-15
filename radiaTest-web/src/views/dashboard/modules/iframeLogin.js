8f8f622a464d6d02ff2be48a7c80eec5aeb37501
liting <2436044320@qq.com>
2023-05-15 16:02:12 +0800

    raidaTest嵌入majun登录整改

--- radiaTest-web/src/views/dashboard/modules/iframeLogin.js
+++ radiaTest-web/src/views/dashboard/modules/iframeLogin.js
@@ -1,37 +1,19 @@
-import { hanleLogin } from '@/views/login/modules/login';
 import { urlArgs } from '@/assets/utils/urlUtils';
 import { storage } from '@/assets/utils/storageUtils';
-import { initData } from './dashboard';
-
-function getIframeMessage(val) {
-  const params = val ? val.data : {};
-  if(params.orgId) {
-    hanleLogin(params.orgId);
-  }
-  if(params.url) {
-    storage.setValue('token', params.token);
-    storage.setValue('user_id', params.userId);
-    window.location = params.url;
-  }
-}
-
 function iframeLogin() {
-  const { isIframe, orgId } = urlArgs();
+  const { isIframe, value } = urlArgs();
   if(isIframe && isIframe === '1') {
+    const { orgId, url, token, giteeId } = JSON.parse(decodeURIComponent(value));
     storage.setValue('isIframe', isIframe);
-    window.hideLogout = true;
-    if(orgId) {
-      hanleLogin(orgId);
+    token && storage.setValue('token', token);
+    orgId && storage.setValue('loginOrgId', orgId);
+    giteeId && storage.setValue('user_id', giteeId);
+    if(url) {
+      window.location = url;
     }
-    window.addEventListener('message', e => {
-      getIframeMessage(e);
-    });
-  } else {
-    initData();
   }
 }
 
 export {
-  iframeLogin,
-  getIframeMessage
+  iframeLogin
 };
