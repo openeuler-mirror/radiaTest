0460584f26012cd5059a5e5080c6b044bcfa3a01
liting <2436044320@qq.com>
2023-05-19 18:06:33 +0800

    radiaTest集成majun登录整改--修改

--- radiaTest-web/src/views/dashboard/modules/iframeLogin.js
+++ radiaTest-web/src/views/dashboard/modules/iframeLogin.js
@@ -5,9 +5,9 @@ function iframeLogin() {
   if(isIframe && isIframe === '1') {
     const { orgId, url, token, giteeId } = JSON.parse(decodeURIComponent(value));
     storage.setValue('isIframe', isIframe);
-    token && storage.setValue('token', token);
-    orgId && storage.setValue('loginOrgId', orgId);
-    giteeId && storage.setValue('user_id', giteeId);
+    storage.setValue('token', token);
+    storage.setValue('loginOrgId', orgId);
+    storage.setValue('user_id', giteeId);
     if(url) {
       window.location = url;
     }
