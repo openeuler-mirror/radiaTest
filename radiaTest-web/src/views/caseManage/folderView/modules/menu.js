db7b941efcfb19dd88269d7fcd44e6f31fd3cfda
xujie <1254023616@qq.com>
2023-05-22 15:12:12 +0800

    fix:测试用例标题修改

--- radiaTest-web/src/views/caseManage/folderView/modules/menu.js
+++ radiaTest-web/src/views/caseManage/folderView/modules/menu.js
@@ -1206,6 +1206,7 @@ const actionHandlder = {
       } else if (key === 'case') {
         const id = contextmenu.info.case_id;
         getCaseDetail(id).then((res) => {
+          res.data.name = contextmenu.label;
           store.commit('rowData/set', res.data);
           updateModalRef.value.show();
         });
@@ -1213,6 +1214,8 @@ const actionHandlder = {
     }
   }
 };
+
+// 点击右键菜单
 function selectAction({ contextmenu, action }) {
   actionHandlder[action.key].handler(contextmenu);
 }
