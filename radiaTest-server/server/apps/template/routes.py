51b52a2147c55e5badc810af37fbfddfdab8326c
mazenggang3 <414659165@qq.com>
2023-07-07 14:32:04 +0800

    feat:测试执行模板创建和修改支持测试用例文件导入和手动选择两种方式.

--- radiaTest-server/server/apps/template/routes.py
+++ radiaTest-server/server/apps/template/routes.py
@@ -7,12 +7,13 @@
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
 ####################################
-# @Author  : 
-# @email   : 
+# @Author  :
+# @email   :
 # @Date    : 2023-05-24
 # @License : Mulan PSL v2
 #####################################
 
+import json
 from datetime import datetime
 import pytz
 from flask import request, jsonify, g
@@ -45,34 +46,47 @@ class TemplateEvent(Resource):
         if template:
             return jsonify(error_code=RET.DATA_EXIST_ERR, error_msg="the template exists")
 
-        if not request.files.get("file"):
+        if request.files.get("file") and request.form.get("cases"):
             return jsonify(
-                error_code=RET.PARMA_ERR, 
-                error_msg="The file being uploaded is not exist"
+                error_code=RET.PARMA_ERR,
+                error_msg=f"file and cases cannot exist at the same time"
             )
-        _filetype = request.files.get("file").filename.split(".")[-1]
-        if _filetype not in ["xls", "csv", "xlsx", "json"]:
-            return jsonify(
-                error_code=RET.PARMA_ERR, 
-                error_msg=f"Filetype of {_filetype} not supported"
-            )
+        elif request.files.get("file"):
+            _filetype = request.files.get("file").filename.split(".")[-1]
+            if _filetype not in ["xls", "csv", "xlsx", "json"]:
+                return jsonify(
+                    error_code=RET.PARMA_ERR,
+                    error_msg=f"Filetype of {_filetype} not supported"
+                )
 
-        try:
-            import_handler = TemplateCaseImportHandler(request.files.get("file"))
-        except RuntimeError as e:
-            return jsonify(
-                error_code=RET.RUNTIME_ERROR,
-                error_msg=str(e),
+            try:
+                import_handler = TemplateCaseImportHandler(
+                    request.files.get("file"))
+            except RuntimeError as e:
+                return jsonify(
+                    error_code=RET.RUNTIME_ERROR,
+                    error_msg=str(e),
+                )
+            _body = body.__dict__
+            _ = ResourceManager("template").add("api_infos.yaml", _body)
+            import_handler.import_case(
+                {
+                    "name": body.name,
+                    "git_repo_id": body.git_repo_id
+                }
             )
-
-        _body = body.__dict__
-        _ = ResourceManager("template").add("api_infos.yaml", _body)
-        import_handler.import_case(
-            {
-                "name": body.name,
-                "git_repo_id": body.git_repo_id
-            }
-        )
+        elif request.form.get("cases"):
+            _body = body.__dict__
+            _ = ResourceManager("template").add("api_infos.yaml", _body)
+            try:
+                cases = json.loads(request.form.get("cases"))
+            except ValueError:
+                return jsonify(error_code=RET.PARMA_ERR, error_msg="cases param error.")
+            template = Template.query.filter_by(name=body.name).first()
+            TemplateCaseImportHandler.add_template_cases(
+                template=template,
+                cases=cases
+            )
         return jsonify(error_code=RET.OK, error_msg="OK")
 
     @auth.login_required
@@ -93,39 +107,44 @@ class TemplateItemEvent(Resource):
             return jsonify(error_code=RET.DATA_EXIST_ERR, error_msg="the template doesn't exist")
 
         if request.form.get("name"):
-            _template = Template.query.filter_by(name=request.form.get("name")).first()
+            _template = Template.query.filter_by(
+                name=request.form.get("name")).first()
             if _template and _template.id != template_id:
                 return jsonify(error_code=RET.DATA_EXIST_ERR, error_msg="the template name exists")
-        
+
         _form = dict()
         for key, value in request.form.items():
             if value:
                 _form[key] = value
-                setattr(template, key, value)
+        body = TemplateUpdate(**_form)
+        _body = body.__dict__
+        for key, value in _body.items():
+            setattr(template, key, value)
         template.add_update()
 
-        if request.files.get("file"):
+        if request.files.get("file") and request.form.get("cases"):
+            return jsonify(
+                error_code=RET.PARMA_ERR,
+                error_msg=f"file and cases cannot exist at the same time"
+            )
+        elif request.files.get("file"):
             _filetype = request.files.get("file").filename.split(".")[-1]
             if _filetype not in ["xls", "csv", "xlsx", "json"]:
                 return jsonify(
-                    error_code=RET.PARMA_ERR, 
+                    error_code=RET.PARMA_ERR,
                     error_msg=f"Filetype of {_filetype} not supported"
                 )
 
             try:
-                import_handler = TemplateCaseImportHandler(request.files.get("file"))
+                import_handler = TemplateCaseImportHandler(
+                    request.files.get("file")
+                )
             except RuntimeError as e:
                 return jsonify(
                     error_code=RET.RUNTIME_ERROR,
                     error_msg=str(e),
                 )
-
-            for case in template.cases:
-                template.cases.remove(case)
-                template.add_update()
-            #循环删除，会遗留一个，单独删除遗留的一个
-            template.cases.remove(template.cases[0])
-            template.add_update()
+            TemplateCaseImportHandler.remove_template_cases(template)
 
             import_handler.import_case(
                 {
@@ -133,15 +152,23 @@ class TemplateItemEvent(Resource):
                     "git_repo_id": template.git_repo_id
                 }
             )
+        elif request.form.get("cases"):
+            try:
+                cases = json.loads(request.form.get("cases"))
+            except ValueError:
+                return jsonify(error_code=RET.PARMA_ERR, error_msg="cases param error.")
+            TemplateCaseImportHandler.update_template_cases(
+                template=template,
+                cases=cases
+            )
         return jsonify(error_code=RET.OK, error_msg="OK")
 
     @auth.login_required
-    @casbin_enforcer.enforcer
     def get(self, template_id):
         template = Template.query.filter_by(id=template_id).first()
         if not template:
             return jsonify(error_code=RET.NO_DATA_ERR, error_msg="template not exist")
-        
+
         vm_req_num = 0
         pm_req_num = 0
         for case in template.cases:
@@ -157,7 +184,7 @@ class TemplateItemEvent(Resource):
         })
 
         return jsonify(error_code=RET.OK, error_msg="OK", data=return_data)
-    
+
     @auth.login_required
     @validate()
     @casbin_enforcer.enforcer
@@ -172,7 +199,9 @@ class TemplateCloneEvent(Resource):
         _template = Template.query.filter_by(id=body.id).first()
         if not _template:
             raise RuntimeError("the selected template does not exist.")
-        _nowstr = datetime.now(tz=pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d-%H-%M-%S")
+        _nowstr = datetime.now(
+            tz=pytz.timezone('Asia/Shanghai')
+        ).strftime("%Y-%m-%d-%H-%M-%S")
         _body = {
             "name": _template.name + _nowstr,
             "description": _template.description + _nowstr if _template.description else _nowstr,
@@ -183,10 +212,12 @@ class TemplateCloneEvent(Resource):
             "group_id": body.group_id,
             "org_id": _template.org_id,
         }
-       
+
         resp = ResourceManager("template").add("api_infos.yaml", _body)
-        
-        template = Template.query.filter_by(name=_template.name + _nowstr).first()
+
+        template = Template.query.filter_by(
+            name=_template.name + _nowstr
+        ).first()
 
         for case in _template.cases:
             template.cases.append(case)
