8ede76751dcce40f898931fdde45537c8b2e5942
hu_kun <710686563@qq.com>
2023-11-27 18:03:35 +0800

    修改at结果获取方式

--- radiaTest-server/server/apps/external/handler.py
+++ radiaTest-server/server/apps/external/handler.py
@@ -18,10 +18,14 @@
 import re
 import datetime
 import json
+from contextlib import contextmanager
 
+import redis
+import requests
 from flask import current_app, jsonify, request
 
 
+from celeryservice.tasks import read_openqa_tests_overview
 from server.model.product import Product
 from server.model.milestone import Milestone
 from server.model.testcase import Suite, CaseNode
@@ -227,141 +231,233 @@ class AtHandler:
         self.at_messenger = at_messenger
         self.buildname_x86 = buildname_x86 if buildname_x86 else ""
         self.buildname_aarch64 = buildname_aarch64 if buildname_aarch64 else ""
+        self.openqa_url = current_app.config.get("OPENQA_URL")
+
+    @staticmethod
+    def parse_url(iso_url):
+        name_list = iso_url.split("/")
+        iso_name = name_list[-1]
+        arch = name_list[-2]
+        os_name = iso_name.split(f"-{arch}")[0]
+        product = os_name.split("-")[0]
+        version = os_name.split(f"{product}-")[-1]
+        ret = re.search(r"\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}", iso_url)
+        return {
+            "product": product,
+            "version": version,
+            "arch": arch,
+            "build": ret.group(),
+        }
+
+    @contextmanager
+    def get_scrapy_redis_cli(self):
+        scrapyspider_pool = redis.ConnectionPool.from_url(
+            current_app.config.get("SCRAPYSPIDER_BACKEND"),
+            decode_responses=True
+        )
+        scrapy_redis_cli = redis.StrictRedis(connection_pool=scrapyspider_pool)
+        try:
+            yield scrapy_redis_cli
+        finally:
+            scrapyspider_pool.disconnect()
+
+    def get_build_info(self):
+        build_list = list()
+        arch_list = []
+        if self.buildname_x86:
+            x86_parse_info = self.parse_url(self.buildname_x86)
+            arch_list.append(x86_parse_info.pop("arch"))
+            build_list.append(x86_parse_info)
+        if self.buildname_aarch64:
+            arm_parse_info = self.parse_url(self.buildname_aarch64)
+            arch_list.append(arm_parse_info.pop("arch"))
+            build_list.append(arm_parse_info)
+
+        if len(build_list) == 2:
+            if build_list[0] == build_list[1]:
+                parse_info = build_list[0]
+                product = parse_info.get("product")
+                version = parse_info.get("version")
+                build = parse_info.get("build")
+            else:
+                return False, {}
+
+        else:
+            parse_info = build_list[0]
+            product = parse_info.get("product")
+            version = parse_info.get("version")
+            build = parse_info.get("build")
+
+        return True, {
+            "product": product,
+            "version": version,
+            "arch_list": arch_list,
+            "build": build,
+        }
 
     def get_result(self):
-        build_list = list()
         at_result = dict()
-        if self.buildname_x86:
-            build_list.append(self.buildname_x86)
-        if self.buildname_aarch64:
-            build_list.append(self.buildname_aarch64)
-        at_jobs = AtJob.query.filter(AtJob.build_name.in_(build_list)).all()
+        flag, build_info = self.get_build_info()
+        if flag is False:
+            at_result.update({
+                "result": "url err",
+                "openqa_url": "",
+                "rate": 0
+            })
+            return True, at_result
+        # 获取tests_overview_url 查询所有结果
+        product = build_info.get("product")
+        version = build_info.get("version")
+        arch_list = build_info.get("arch_list")
+        build = build_info.get("build")
 
-        if len(at_jobs) == 0:
+        tests_overview_url = f"/tests/overview?distri={product.lower()}&version={version}&build=-{build}"
+        # 校验是否已触发at任务
+        res = requests.get(f"{self.openqa_url}{tests_overview_url}")
+        # 不存在直接返回
+        if res.status_code != 200:
             at_result.update({
                 "result": "no triggered",
                 "openqa_url": "",
                 "rate": 0
             })
             return True, at_result
-        else:
-            total_job_num = 0
-            job_success_num = 0
-            job_fail_num = 0
-            job_cancel_num = 0
-
-            for job in at_jobs:
-                if not job.at_job_ids:
-                    continue
-                at_job_ids = job.at_job_ids.split(",")
-
-                for job_id in at_job_ids:
-                    job_detail = self.at_messenger.get_job_detail(job_id)
-                    # 排除异常job_id
-                    if not job_detail:
-                        continue
-                    total_job_num += 1
-                    state = job_detail.get("state")
-                    result = job_detail.get("result")
-                    # 任务状态done且结果为passed执行成功
-                    if state == "done":
-                        if "passed" == result:
-                            job_success_num += 1
-                        else:
-                            job_fail_num += 1
-                    elif state == "cancelled":
-                        job_cancel_num += 1
-                    else:
-                        continue
-
-            openqa_url = "https://radiatest.openeuler.org/at-detail?release_url_x86_64={}" \
-                         "&release_url_aarch64={}".format(self.buildname_x86, self.buildname_aarch64)
-            job_done_num = job_success_num + job_fail_num + job_cancel_num
-
-            if total_job_num == 0:
+        # 存在则获取redis结果
+        with self.get_scrapy_redis_cli() as scrapy_redis_cli:
+            # 获取所有结果keys
+            product_build = f"{product}_{version}_Build-{build}"
+            all_test_keys = scrapy_redis_cli.keys(f"{product_build}_*")
+            if len(all_test_keys) == 0:
+                # 异步执行爬虫爬取数据
+                read_openqa_tests_overview.delay(
+                    product_build=product_build,
+                    tests_overview_url=tests_overview_url
+                )
                 at_result.update({
-                    "result": "in process",
+                    "result": "in crawling",
                     "openqa_url": "",
                     "rate": 0
                 })
                 return True, at_result
-            elif total_job_num != job_done_num:
-                at_result.update({
-                    "result": "in process",
-                    "openqa_url": openqa_url,
-                    "rate": round(job_done_num / total_job_num, 2)
-                })
-                return True, at_result
             else:
-                if job_fail_num == 0 and job_cancel_num == 0:
-                    result = "passed"
+                total_job_num = 0
+                job_success_num = 0
+                job_fail_num = 0
+                job_cancel_num = 0
+                # 从redis遍历获取所有结果
+                for test_key in all_test_keys:
+                    for arch in arch_list:
+                        res_status = scrapy_redis_cli.hget(test_key, f"{arch}_res_status")
+                        if "-" == res_status or res_status is None:
+                            continue
+                        if ":" in res_status:
+                            res_list = res_status.split(": ")
+                            state = res_list[0].lower()
+                            result = res_list[1].lower()
+                        else:
+                            state = res_status.lower()
+                            result = ""
+
+                        total_job_num += 1
+                        # 任务状态done且结果为passed执行成功
+                        if state == "done":
+                            if "passed" == result:
+                                job_success_num += 1
+                            else:
+                                job_fail_num += 1
+                        elif state == "cancelled":
+                            job_cancel_num += 1
+                        else:
+                            continue
+
+                openqa_url = "https://radiatest.openeuler.org/at-detail?release_url_x86_64={}" \
+                             "&release_url_aarch64={}".format(self.buildname_x86, self.buildname_aarch64)
+                job_done_num = job_success_num + job_fail_num + job_cancel_num
+
+                if total_job_num == 0:
+                    at_result.update({
+                        "result": "in process",
+                        "openqa_url": "",
+                        "rate": 0
+                    })
+                    return True, at_result
+                elif total_job_num != job_done_num:
+                    at_result.update({
+                        "result": "in process",
+                        "openqa_url": openqa_url,
+                        "rate": round(job_done_num / total_job_num, 2)
+                    })
+                    return True, at_result
                 else:
-                    result = "failed"
+                    if job_fail_num == 0 and job_cancel_num == 0:
+                        result = "passed"
+                    else:
+                        result = "failed"
 
-                at_result.update({
-                    "result": result,
-                    "openqa_url": openqa_url,
-                    "rate": 1
-                })
+                    at_result.update({
+                        "result": result,
+                        "openqa_url": openqa_url,
+                        "rate": 1
+                    })
 
-                return True, at_result
+                    return True, at_result
 
     def get_report(self):
-        build_list = list()
-        detail_dict = dict()
-        if self.buildname_x86:
-            build_list.append(self.buildname_x86)
-        if self.buildname_aarch64:
-            build_list.append(self.buildname_aarch64)
-        at_jobs = AtJob.query.filter(AtJob.build_name.in_(build_list)).all()
+        flag, build_info = self.get_build_info()
         total_job_num = 0
         job_success_num = 0
         job_fail_num = 0
         job_cancel_num = 0
-
-        for job in at_jobs:
-            if not job.at_job_ids:
-                continue
-            at_job_ids = job.at_job_ids.split(",")
-
-            for job_id in at_job_ids:
-                job_detail = self.at_messenger.get_job_detail(job_id)
-                # 排除异常job_id
-                if not job_detail:
-                    continue
-                total_job_num += 1
-                settings = job_detail.get("settings")
-                arch = settings.get("ARCH")
-                testcase = settings.get("TEST")
-                state = job_detail.get("state")
-                result = job_detail.get("result")
-                if testcase not in detail_dict:
-                    detail_dict[testcase] = {}
-                if arch in detail_dict[testcase]:
-                    current_app.logger.warning(f"{job_id} arch is repeat!")
-                detail_dict[testcase][arch] = {
-                    "state": state,
-                    "result": result
-                }
-                # 任务状态done且结果为passed执行成功
-                if state == "done":
-                    if "passed" == result:
-                        job_success_num += 1
-                    else:
-                        job_fail_num += 1
-                elif state == "cancelled":
-                    job_cancel_num += 1
-                else:
-                    continue
         detail_list = []
-        arch_list = []
-        for testcase, case_info in detail_dict.items():
-            detail = {"test": testcase}
-            for arch, info in case_info.items():
-                if arch not in arch_list:
-                    arch_list.append(arch)
-                detail[arch] = info
-            detail_list.append(detail)
+        arch_list = build_info.get("arch_list", [])
+        if flag is True:
+            # 获取tests_overview_url 查询所有结果
+            product = build_info.get("product")
+            version = build_info.get("version")
+            build = build_info.get("build")
+            detail_dict = dict()
+            # 获取存在redis结果
+            with self.get_scrapy_redis_cli() as scrapy_redis_cli:
+                # 获取所有结果keys
+                product_build = f"{product}_{version}_Build-{build}"
+                all_test_keys = scrapy_redis_cli.keys(f"{product_build}_*")
+                for test_key in all_test_keys:
+                    for arch in arch_list:
+                        testcase = test_key.split(f"{product_build}_")[-1]
+                        res_status = scrapy_redis_cli.hget(test_key, f"{arch}_res_status")
+                        if "-" == res_status or res_status is None:
+                            continue
+                        if ":" in res_status:
+                            res_list = res_status.split(": ")
+                            state = res_list[0].lower()
+                            result = res_list[1].lower()
+                        else:
+                            state = res_status.lower()
+                            result = ""
+                        total_job_num += 1
+                        if testcase not in detail_dict:
+                            detail_dict[testcase] = {}
+                        if arch in detail_dict[testcase]:
+                            current_app.logger.warning(f"{product_build} {testcase} {arch} is repeat!")
+                        detail_dict[testcase][arch] = {
+                            "state": state,
+                            "result": result
+                        }
+                        # 任务状态done且结果为passed执行成功
+                        if state == "done":
+                            if "passed" == result:
+                                job_success_num += 1
+                            else:
+                                job_fail_num += 1
+                        elif state == "cancelled":
+                            job_cancel_num += 1
+                        else:
+                            continue
+            for testcase, case_info in detail_dict.items():
+                detail = {"test": testcase}
+                for arch, info in case_info.items():
+                    detail[arch] = info
+                detail_list.append(detail)
         return {
             "total": total_job_num,
             "success": job_success_num,
